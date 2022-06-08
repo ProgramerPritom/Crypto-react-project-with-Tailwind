424e392a307f91a7854db590363a8bf6cf211dbc
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-06-08 09:01:36 +0600

    Crypto coin project setup successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './components/Homepage/Homepage'
+
 
 const App = () => {
   return (
-    <div>
-      <h1>Crypto Cafe Starter Template</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Homepage></Homepage>}></Route>
+    </Routes>
   )
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
+            <div className='hidden md:block'>Crypto Cafe</div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Header;
