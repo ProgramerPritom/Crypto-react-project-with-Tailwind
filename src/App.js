1092f19c70c3179329b289633edfc0c325428983
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-06-08 09:33:14 +0600

    Create custom link and setup with route navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import About from './components/About/About'
+import Coins from './components/Coins/Coins'
+import Contact from './components/Contact/Contact'
+import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
 
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Homepage></Homepage>}></Route>
-    </Routes>
+    <div>
+          <Header></Header>
+        <Routes>
+          <Route path='/' element={<Homepage></Homepage>}></Route>
+          <Route path='/coin' element={<Coins></Coins>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+        </Routes>
+    </div>
+    
   )
 }
 
---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coins = () => {
+    return (
+        <div>
+            <h2>This is coin page!</h2>
+        </div>
+    );
+};
+
+export default Coins;
